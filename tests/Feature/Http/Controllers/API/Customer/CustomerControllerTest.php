b4ed472175b524a393a104570d6041f6359390fb
Parham <parham.parnian@gmail.com>
2023-02-07 13:23:27 +0330

    #10 Edit test cases implemented

--- tests/Feature/Http/Controllers/API/Customer/CustomerControllerTest.php
+++ tests/Feature/Http/Controllers/API/Customer/CustomerControllerTest.php
@@ -142,4 +142,138 @@ class CustomerControllerTest extends TestCase
                 ]
             ]);
     }
+
+
+    public function test_admin_can_update_customer_using_valid_inputs()
+    {
+
+        $customer = Customer::factory()->create();
+        $payload = [
+            'first_name'   => $this->faker()->name(),
+            'last_name'    => $this->faker()->lastName(),
+            'email'        => $this->faker()->email(),
+            'phone_number' => "09123456789",
+            'password'     => "Pp1234567!",
+        ];
+        $response = $this->json("PUT", route('admin.customers.update', $customer->id), $payload);
+        $response->assertSuccessful()
+            ->assertStatus(200)
+            ->assertJson([
+                "data" => [
+                    'first_name'   => $payload['first_name'],
+                    'last_name'    => $payload['last_name'],
+                    'email'        => $payload['email'],
+                    'phone_number' => $payload['phone_number'],
+                    'is_suspend'   => 'active',
+                    'last_login'   => null,
+                ]
+            ]);
+    }
+    public function test_admin_can_not_update_customer_without_required_inputs()
+    {
+        $customer = Customer::factory()->create();
+        $payload  = [];
+        $response = $this->json("PUT", route('admin.customers.update', $customer->id), $payload);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                "message",
+                "errors" => [
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "phone_number",
+                    "password",
+                ]
+            ]);
+    }
+    public function test_admin_can_not_update_customer_without_unique_email()
+    {
+        $customer = Customer::factory()->count(2)->create();
+        $payload  = [
+            'first_name'   => $this->faker()->name(),
+            'last_name'    => $this->faker()->lastName(),
+            'email'        => $customer[1]->email,
+            'phone_number' => "09123456789",
+            'password'     => "Pp1234567!",
+        ];
+        $response = $this->json("PUT", route('admin.customers.update', $customer->first()->id), $payload);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                "message",
+                "errors" => [
+                    "email",
+                ]
+            ]);
+    }
+    public function test_admin_can_not_update_customer_with_invalid_phone_number()
+    {
+        $customer = Customer::factory()->create();
+        $payload = [
+            'first_name'   => $this->faker()->name(),
+            'last_name'    => $this->faker()->lastName(),
+            'email'        => $this->faker()->email(),
+            'phone_number' => "9123456789",
+            'password'     => "Pp1234567!",
+        ];
+
+
+        $response = $this->json("PUT", route('admin.customers.update', $customer->id), $payload);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                "message",
+                "errors" => [
+                    "phone_number",
+                ]
+            ]);
+    }
+    public function test_admin_can_not_update_customer_with_invalid_suspend_value()
+    {
+        $customer = Customer::factory()->create();
+        $payload = [
+            'first_name'   => $this->faker()->name(),
+            'last_name'    => $this->faker()->lastName(),
+            'email'        => $this->faker()->email(),
+            'is_suspend'   => $this->faker()->name(),
+            'phone_number' => "09123456789",
+            'password'     => "Pp1234567!",
+        ];
+
+
+        $response = $this->json("PUT", route('admin.customers.update', $customer->id), $payload);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                "message",
+                "errors" => [
+                    "is_suspend",
+                ]
+            ]);
+    }
+    public function test_admin_can_not_update_customer_with_invalid_password_value()
+    {
+        $customer = Customer::factory()->create();
+        $payload = [
+            'first_name'   => $this->faker()->name(),
+            'last_name'    => $this->faker()->lastName(),
+            'email'        => $this->faker()->email(),
+            'phone_number' => "09123456789",
+            'password'     => "1234",
+        ];
+        $response = $this->json("PUT", route('admin.customers.update', $customer->id), $payload);
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                "message",
+                "errors" => [
+                    "password",
+                ]
+            ]);
+    }
 }
