48b918d540414df43a790c62909d8c97e7fad062
RasoulBassami <rasoul.bassami@gmail.com>
2023-02-08 15:46:44 +0330

    #16 test: refactor image model tests

--- tests/Feature/Models/ImageTest.php
+++ tests/Feature/Models/ImageTest.php
@@ -13,23 +13,6 @@ class ImageTest extends TestCase
 {
     use WithFaker, RefreshDatabase;
 
-    /**
-     * Setup the test environment.
-     *
-     * @return void
-     */
-    public function setUp() :void
-    {
-        parent::setUp();
-
-        $this->customer = Customer::factory()->create();
-//        $this->products = Product::factory()->create(['user_id' => $this->customer->id]);
-        $this->image = Image::factory()->create([
-            "imageable_id" => $this->customer->id,
-            "imageable_type" => "App\Models\Customer",
-        ]);
-    }
-
     public function test_images_table_has_expected_columns()
     {
         $this->assertTrue(Schema::hasColumns('images', [
@@ -39,11 +22,19 @@ class ImageTest extends TestCase
 
     public function test_an_image_can_be_morphed_to_a_customer_model()
     {
-        $this->assertInstanceOf(Customer::class, $this->image->imageable);
+        $image = Image::factory()
+            ->hasImageable(Customer::factory())
+            ->create();
+
+        $this->assertInstanceOf(Customer::class, $image->imageable);
     }
 
     public function test_a_customer_can_morphs_many_images()
     {
-        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $this->customer->images);
+        $count = rand(2,10);
+        $customer = Customer::factory()->hasImages($count)->create();
+
+        $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $customer->images);
+        $this->assertCount($count, $customer->images);
     }
 }
