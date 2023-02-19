eab16855e6305c381527ede9d23e8bb0db2be4dd
Mohammad Hosein Ronaghi <mhrownaghi@gmail.com>
2023-02-19 18:15:31 +0330

    #23 create cart item model

---
+++ database/migrations/2023_02_19_143928_create_cart_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('qty');
+            $table->string('name');
+            $table->unsignedInteger('price');
+            $table->foreignId('cart_id');
+            $table->foreignId('product_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
